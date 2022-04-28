8eb51ab7abbae858bdea9faffa3e1ed1c71d6cb2
denys.horovyi <denys.horovyi@globallogic.com>
2022-04-28 14:00:14 +0200

    Lesson 2,3
    - l_2_o_n_operations.py has been created

---
+++ src/l_2_o_n_operations.py
@@ -0,0 +1,13 @@
+def check_prime(number):
+    num_iterations = 0
+    for i in range(2, number):
+        num_iterations += 1
+
+        if number % i == 0:
+            print(f'{number} is not a prime number '
+                  f'\nTotal number of iterations = {num_iterations}')
+
+    print(f'{number} is a prime number '
+          f'\nTotal number of iterations = {num_iterations}')
+
+check_prime(88)
