895c0701faacb21bc2b3eb77030ad2b2429ee558
denys.horovyi <denys.horovyi@globallogic.com>
2022-04-28 14:58:46 +0200

    Lesson 7
    - l_7_stack.py has been created
    - basic methods of stack have been observed

---
+++ src/stack_investigation/l_7_stack.py
@@ -0,0 +1,17 @@
+stack = []
+
+stack.append('United States(USA)')
+stack.append('Great Britain(GBR)')
+stack.append('China(CHN)')
+
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+stack.append('United States(USA)')
+stack.append('Great Britain(GBR)')
+stack.append('China(CHN)')
+
+print(stack)
